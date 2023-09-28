763b9fac188c091763610ff6e1673dfbf50c76b3
Sanjida_Nasrin_Shokhi <143315231+sanjidashokhi@users.noreply.github.com>
2023-09-29 01:18:35 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,58 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'fonts': ['Open Sans', 'sans-serif'],
+                'fontsnunti': ['Nunito Sans', 'sans-serif'],
+                'fontspaprik': ['Paprika', 'cursive'],
+                'fontsquick': ['Quicksand', 'sans-serif'],
+            },
+            lineHeight: {
+                'hight': ['100px'],
+                'greenfont': ['64px'],
+                'lastone': ['55px']
+
+            },
+            letterSpacing: {
+                lattersss: ['10px'],
+                latters: ['0.5px'],
+                newsss: ['-1px'],
+                lastone: ['-1.8px']
+
+            },
+            padding: {
+                'pdsssss': '319px'
+            },
+            colors: {
+                'iconcol': '#fff'
+            },
+            colors: {
+                'navcolr': 'rgba(22, 12, 109, 0.50)',
+                'banneroverly': 'rgba(106, 98, 197, 0.90)',
+                'linecolr': 'rgba(79, 88, 95, 0.2)',
+                'littlebox': 'rgb(247, 247, 247)',
+                'littleboxmid': 'rgba(247, 247, 247, 0.301)',
+                'footfont': 'rgba(254, 254, 254, 0.9)',
+
+                
+              },
+              fontSize: {
+                fonsiz: ['84px'],
+               
+              },
+              backgroundImage: {
+                'bannerimgone': "url('./images/bagbannim.png')",
+                'gallabg': "url('./images/gallerybg.png')",
+                'gallabgtwo': "url('./images/gallerybg (2).png')",
+                'yellogell': "url('./images/yellogallary.png')",
+                'purplebacg': "url('./images/purplebg.png')",
+                
+              }
+              
+
+        
+
+
+        }
+    }
+}
